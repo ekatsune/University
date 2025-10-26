cc0247cdc2857198af54253d2e791df9cdd1e4f6
ekatsune <suninmyhous@gmail.com>
2025-10-26 21:51:45 +0300

    fgsn

---
+++ ThePostMachine/cmake-build-debug-coverage/Google_tests/lib/googletest/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: E:/Third Semestr/PPOIS_lb/laba №1/ThePostMachine/Google_tests/lib/googletest
+# Build directory: E:/Third Semestr/PPOIS_lb/laba №1/ThePostMachine/cmake-build-debug-coverage/Google_tests/lib/googletest
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
